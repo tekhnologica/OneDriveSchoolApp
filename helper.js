4f7ea324da27cf140b7c9cab4eeed213ed473dc0
Cody <tekhnologica@yahoo.com>
2014-09-27 21:17:11 -0400

    site changes

--- helper.js
+++ helper.js
@@ -1,3 +1,26 @@
 $(document).ready(function () {
-    alert('yay');
+
+    WL.init({
+        client_id: '000000004012F40E',
+        redirect_uri: 'http://ec2-54-69-112-56.us-west-2.compute.amazonaws.com/landing.html'
+    });
+
+    $("#statusDiv").hide();
+    $("#loginBtn").click(function () {
+        WL.login({
+            scope: ["wl.skydrive", "wl.signin"]
+        }).then(
+            function (resonse) {
+                console.log(response);
+                $("#statusText").html("Login Succeeded!");
+                $("#statusText").css({ 'color': 'green' });
+            },
+            function (badresponse) {
+                console.log(badresponse);
+                $("#statusText").html("Login Failed...");
+                $("#statusText").css({ 'color': 'red' });
+                $("#statusText").html("Not Logged In").delay(3000);
+                $("#statusText").css({ 'color': 'blue' }).delay(3000);
+            })
+    });
 });
