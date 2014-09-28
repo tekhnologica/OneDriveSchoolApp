63cab7584c855f9401fc0f90cd7b64c9282f9ee1
Cody <tekhnologica@yahoo.com>
2014-09-27 23:20:14 -0400

    site changes

--- landing_helper.js
+++ landing_helper.js
@@ -2,8 +2,7 @@ $(document).ready(function () {
 
     WL.init({
         client_id: '000000004012F40E',
-        redirect_uri: 'http://ec2-54-69-112-56.us-west-2.compute.amazonaws.com/landing.html',
-        scope: ['wl.skydrive', 'wl.basic']
+        redirect_uri: 'http://ec2-54-69-112-56.us-west-2.compute.amazonaws.com/landing.html'
     });
 
     $("#loadBtn").click(function () {
