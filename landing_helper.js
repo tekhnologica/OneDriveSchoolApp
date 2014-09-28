e001b29d8cc175c7624c09e1ccd259d315021d89
Cody <tekhnologica@yahoo.com>
2014-09-27 23:22:41 -0400

    site changes

--- landing_helper.js
+++ landing_helper.js
@@ -4,13 +4,18 @@ $(document).ready(function () {
         client_id: '000000004012F40E',
         redirect_uri: 'http://ec2-54-69-112-56.us-west-2.compute.amazonaws.com/landing.html'
     });
+    try {
+        var sess = WL.getSession().access_token;
+    }
+    catch (err) {
+        console.log('error: unable to get session data');
+    }
 
     $("#loadBtn").click(function () {
-        console.log(WL.getSession());
         WL.api({
             path: 'me',
             method: 'GET',
-            access_token: WL.getSession().access_token
+            access_token: sess.access_token
         }).then(
             function (response) {
                 console.log(response);
