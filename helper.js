39cb7499b10f7394cf2229d258e8d34fde800d9d
Cody <tekhnologica@yahoo.com>
2014-09-27 23:34:18 -0400

    site changes

--- helper.js
+++ helper.js
@@ -1,7 +1,3 @@
-function loadLanding() {
-    window.location.href = "landing.html";
-};
-
 $(document).ready(function () {
 
     WL.init({
@@ -14,7 +10,7 @@ $(document).ready(function () {
         sess = WL.getSession();
         $("#statusText").html("You are already logged in! Redirecting to landing page...");
         $("#statusText").css({ 'color': 'green' });
-        setTimeout(loadLanding, 4000);
+        setTimeout(function () {window.location.href = "landing.html#accessToken=" + sess.access_token}, 4000);
     }
     catch (err) {
         sess = null;
--- landing_helper.js
+++ landing_helper.js
@@ -1,15 +1,17 @@
+function parseURLParams() {
+    var sliced = window.location.href.slice(window.location.href.indexOf('#') + 1)[0].split('&');
+    console.log(sliced);
+}
+
 $(document).ready(function () {
 
     WL.init({
         client_id: '000000004012F40E',
         redirect_uri: 'http://ec2-54-69-112-56.us-west-2.compute.amazonaws.com/landing.html'
     });
-    try {
-        var sess = WL.getSession().access_token;
-    }
-    catch (err) {
-        console.log('error: unable to get session data');
-    }
+    var sess = WL.getSession();
+    parseURLParams();
+    console.log(sess);
 
     $("#loadBtn").click(function () {
         WL.api({
