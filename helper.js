0da0cf2af6621ab0dbee87b8bff8a2646bb451b1
Cody <tekhnologica@yahoo.com>
2014-09-27 21:55:19 -0400

    site changes

--- helper.js
+++ helper.js
@@ -12,15 +12,9 @@ $(document).ready(function () {
         }).then(
             function (resonse) {
                 console.log(response);
-                $("#statusText").html("Login Succeeded!");
-                $("#statusText").css({ 'color': 'green' });
             },
             function (badresponse) {
                 console.log(badresponse);
-                $("#statusText").html("Login Failed...");
-                $("#statusText").css({ 'color': 'red' });
-                $("#statusText").html("Not Logged In").delay(3000);
-                $("#statusText").css({ 'color': 'blue' }).delay(3000);
             })
     });
 });
